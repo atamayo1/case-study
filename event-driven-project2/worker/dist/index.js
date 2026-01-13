315e23dee509cb721b53027d8cc1d5f9224cf504
Anthony Tamayo Ortega <anthony1_144@hotmail.com>
2026-01-12 19:37:40 -0500

    i added the git ignore

--- event-driven-project2/worker/dist/index.js
+++ event-driven-project2/worker/dist/index.js
@@ -1,11 +1,11 @@
 import { RabbitConsumer } from "./adapters/rabbit/RabbitConsumer.js";
 import { RabbitPublisher } from "./adapters/rabbit/RabbitPublisher.js";
 import { MessageProcessor } from "./app/MessageProcessor.js";
-async function bootstrap() {
+const bootstrap = async () => {
     const resultPublisher = new RabbitPublisher();
     await resultPublisher.connect();
     const processor = new MessageProcessor(resultPublisher);
     const consumer = new RabbitConsumer(processor);
     await consumer.start();
-}
+};
 bootstrap();
